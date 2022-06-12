1fb3a881ac43147bd614728c4673d7f1564e0829
proin <proin@season.co.kr>
2022-06-13 01:42:52 +0900

    mobile

---
+++ config/wiz.py
@@ -0,0 +1,6 @@
+category = [
+    {'id': 'sample.nav', 'title': 'sample/nav'},
+    {'id': 'sample.side', 'title': 'sample/side'},
+    {'id': 'tutorial', 'title': 'tutorial'},
+    {'id': 'modal', 'title': 'Modal'}
+]
