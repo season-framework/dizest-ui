9d3fd39cfa5c41d43024adef021997005e16eab7
proin <proin@season.co.kr>
2022-08-23 15:36:56 +0900

    v2022.08.23.1536

--- apps/admin.release/view.js
+++ apps/admin.release/view.js
@@ -1,7 +1,7 @@
 let wiz_controller = async ($sce, $scope, $timeout) => {
     $scope.history = [];
     $scope.history.push({
-        name: "v2022.08.23.1522",
+        name: "v2022.08.23.1536",
         log: [
             "Releases Info",
             "UI / Core version update on web setting"
